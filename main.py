8d9e8e7138b99c0aa0e19aa01810c781ed7cbdb7
naftaliferreira <naftalibarbosaferreira@gmail.com>
2023-11-18 00:35:42 -0300

    Update main.py
    criação da janela principal da aplicação

--- main.py
+++ main.py
@@ -3,3 +3,24 @@ from tkinter import messagebox
 from tkinter import PhotoImage
 import pandas as pd
 import random
+
+# ler arquivo
+df = pd.read_excel('questoes.xlsx')
+questoes = df.sample(n=10).values.tolist()
+
+# interface gráfica
+
+janela = tk.Tk()
+janela.title('Quiz')
+janela.geometry("400x450")
+
+# definição de cores
+
+background_color = "#ECECEC"
+text_color = "#333333"
+button_color = "#4CAF50"
+
+janela.config(bg=background_color)
+janela.option_add('*Font', 'Arial') 
+
+janela.mainloop()
