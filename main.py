80d632172289cadc5f21100cc4e34a390a6e969c
naftaliferreira <naftalibarbosaferreira@gmail.com>
2023-11-18 09:04:10 -0300

    icones e interface
    adição de icones e elementos relacionados a interface

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import random
 
 # ler arquivo
 df = pd.read_excel('questoes.xlsx')
-questoes = df.sample(n=10).values.tolist()
+question = df.sample(n=10).values.tolist()
 
 # interface gráfica
 
@@ -21,6 +21,17 @@ text_color = "#333333"
 button_color = "#4CAF50"
 
 janela.config(bg=background_color)
-janela.option_add('*Font', 'Arial') 
+janela.option_add('*Font', 'Arial')
+
+# icone
+app_icon = PhotoImage(file="icons8-quiz-100.png")
+app_label = tk.Label(janela, image=app_icon, bg=background_color)
+app_label.pack(pady=10)
+
+# componentes da interface
+question_label = tk.Label(janela, text="Pergunta", wraplength=380,
+                          bg=background_color, fg=text_color, font=("Arial", 12, "bold"))
+question_label.pack(pady=20)
+
 
 janela.mainloop()
